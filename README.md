71822aa6c350097edbbde8b5a516c06390f57cdf
Ash Kyd <ash@kyd.com.au>
2021-05-13 08:54:38 +1000

    add CLI and convert back to JS so we can execute it

---
+++ README.md
@@ -0,0 +1,16 @@
+# Markdown to Wordpress XML
+
+Convert a folder of markdown files (in Jekyll/hexo/eleventy style) to a
+Wordpress XML import file.
+
+⚠⚠⚠ This is not ready ⚠⚠⚠
+
+There's some users & urls hard-coded in the XML that's specific to my use case.
+You can use it as-is and adit the output by hand, or submit a PR to make it more
+generic.
+
+Good luck!
+
+```
+node index.js --input /path/to/markdowns
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+const { version } = require("./package.json");
+const { Command } = require("commander");
+const markdownToWordpress = require("./markdownToWordpress");
+
+const program = new Command();
+
+program.version(version);
+
+program
+  .option(
+    "-i, --input <path>",
+    "path where markdown files live (this is not recursive)"
+  )
+  .option(
+    "-o, --output <file>",
+    "file to put Wordpress XML. Defaults to wordpress-import.xml"
+  );
+
+console.log(process.argv);
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!options.input) {
+  program.help();
+  process.exit();
+}
+
+const pathname = path.resolve(process.cwd(), options.input);
+const output = path.resolve(
+  process.cwd(),
+  options.file || "wordpress-import.xml"
+);
+
+const files = fs
+  .readdirSync(pathname)
+  .map((file) => ({
+    path: path.join(pathname, file),
+    slug: path.basename(file, ".md"),
+  }))
+  .filter((file) => !fs.lstatSync(file.path).isDirectory())
+  .filter((file) => file.path.match(/\.md$/))
+  .map((file) => ({
+    ...file,
+    content: fs.readFileSync(file.path, "utf8"),
+  }));
+
+const xml = markdownToWordpress({ fileArray: files });
+
+fs.writeFileSync(output, xml);
